7e9656ba32f2b6841916ea8765a287e61a3cd54a
lastagous <from.iphone5s@gmail.com>
2023-05-29 11:41:31 +0900

    [FIX] Fixed to seach models to json

--- src/app/store/search-json-data.store.ts
+++ src/app/store/search-json-data.store.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@angular/core';
 import { XivapiStore } from 'src/app/store/xivapi.store';
 import { XivapiItemModel } from '../model/xivapi-item.model';
+import relicItemQueryBaseJson from '../../assets/elasticsearch/xivapi-relic-item.json';
+import relicAchievementQueryBaseJson from '../../assets/elasticsearch/xivapi-relic-achievement.json';
+import { QueryModel } from '../model/xviapi-post.model';
 
 @Injectable()
 export class SearchJsonStore {
   private _items: XivapiItemModel[] = [];
+  private _relicItemQueryBase: QueryModel;
+  private _relicAchievementQueryBase: QueryModel;
 
   constructor(private _xivapiStore: XivapiStore) {
+    this._relicItemQueryBase = relicItemQueryBaseJson as QueryModel;
+    this._relicAchievementQueryBase =
+      relicAchievementQueryBaseJson as QueryModel;
     // Get data with the following method when updated to a new version
     // this.searchWeponAchievements();
-    // this.searchWeponItemsForPatch2To5();
+    this.searchWeponItemsForPatch2To5();
     // this.searchWeponItemsForPatch6();
   }
 
@@ -23,41 +31,20 @@ export class SearchJsonStore {
       'マンダヴィルウェポン',
     ];
     names.forEach((name) => {
-      this._xivapiStore
-        .search({
-          indexes: 'achievement',
-          columns:
-            'AchievementCategory.Name_ja,Description_ja,ID,Icon,IconHD,Name,Name_de,Name_en,Name_fr,Name_ja',
-          body: {
-            query: {
-              bool: {
-                must: [
-                  {
-                    wildcard: {
-                      'AchievementCategory.AchievementKind.Name_ja': 'アイテム',
-                    },
-                  },
-                  {
-                    wildcard: {
-                      'AchievementCategory.Name_ja': `${name}`,
-                    },
-                  },
-                ],
-              },
-            },
-            from: 0,
-            size: 1000,
-            sort: [],
-          },
-        })
-        .subscribe(async (searchAchievements) => {
-          console.log(searchAchievements);
-          for (let result of (searchAchievements as any).Results) {
-            await this.sleep(500);
-            await this.searchWeponItemsFromAchievementName(result.Name_ja);
-          }
-          console.log(this._items);
-        });
+      const query = this._relicAchievementQueryBase;
+      query.body.query.bool.must.push({
+        wildcard: {
+          'AchievementCategory.Name_ja': `${name}`,
+        },
+      });
+      this._xivapiStore.search(query).subscribe(async (searchAchievements) => {
+        console.log(searchAchievements);
+        for (let result of (searchAchievements as any).Results) {
+          await this.sleep(500);
+          await this.searchWeponItemsFromAchievementName(result.Name_ja);
+        }
+        console.log(this._items);
+      });
     });
   }
 
@@ -86,202 +73,60 @@ export class SearchJsonStore {
   }
 
   private async searchWeponItem(itemName: string): Promise<void> {
+    const query = this._relicItemQueryBase;
+    query.body.query.bool.must.push({
+      wildcard: {
+        Name_ja: `*${itemName}*`,
+      },
+    });
+    query.body.query.bool.must.push({
+      wildcard: {
+        Description_ja: `[*]`,
+      },
+    });
     return new Promise((resolve, reject) => {
-      this._xivapiStore
-        .search({
-          indexes: 'item',
-          columns:
-            'ClassJobCategory.Name,ClassJobCategory.Name_de,ClassJobCategory.Name_en,ClassJobCategory.Name_fr,ClassJobCategory.Name_ja,ID,Icon,IconID,LevelEquip,LevelItem,Name,Name_de,Name_en,Name_fr,Name_ja,Patch,Url,UrlType',
-          body: {
-            query: {
-              bool: {
-                must: [
-                  {
-                    wildcard: {
-                      Name_ja: `*${itemName}*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `[*]`,
-                    },
-                  },
-                ],
-                must_not: {
-                  wildcard: {
-                    Name_ja: `*レプリカ*`,
-                  },
-                },
-              },
-            },
-            from: 0,
-            size: 700,
-            sort: [],
-          },
-        })
-        .subscribe((searchItems) => {
-          this._items = this._items.concat(
-            (searchItems as any).Results as XivapiItemModel[]
-          );
-          resolve();
-        });
+      this._xivapiStore.search(query).subscribe((searchItems) => {
+        this._items = this._items.concat(
+          (searchItems as any).Results as XivapiItemModel[]
+        );
+        resolve();
+      });
     });
   }
 
   private async searchWeponItemsForPatch2To5(): Promise<void> {
+    const query = this._relicItemQueryBase;
+    query.body.query.bool.must.push({
+      wildcard: {
+        Description_ja: `[*]`,
+      },
+    });
     return new Promise((resolve, reject) => {
-      this._xivapiStore
-        .search({
-          indexes: 'item',
-          columns:
-            'ClassJobCategory.Name,ClassJobCategory.Name_de,ClassJobCategory.Name_en,ClassJobCategory.Name_fr,ClassJobCategory.Name_ja,Description_ja,ID,Icon,IconID,LevelEquip,LevelItem,Name,Name_de,Name_en,Name_fr,Name_ja,Patch,Url,UrlType',
-          body: {
-            query: {
-              bool: {
-                must: [
-                  {
-                    wildcard: {
-                      Description_ja: `[*]`,
-                    },
-                  },
-                ],
-                must_not: [
-                  {
-                    wildcard: {
-                      Name_ja: `*レプリカ*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*防具*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*模造品*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*1回目の装着*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*装備不可*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*友好部族クエスト*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*スカイスチールツール*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*モーエンツール*`,
-                    },
-                  },
-                ],
-              },
-            },
-            from: 0,
-            size: 1000,
-            sort: [],
-          },
-        })
-        .subscribe((searchItems) => {
-          console.log(searchItems);
-          this._items = this._items.concat(
-            (searchItems as any).Results as XivapiItemModel[]
-          );
-          resolve();
-        });
+      this._xivapiStore.search(query).subscribe((searchItems) => {
+        console.log(searchItems);
+        this._items = this._items.concat(
+          (searchItems as any).Results as XivapiItemModel[]
+        );
+        resolve();
+      });
     });
   }
 
   private async searchWeponItemsForPatch6(): Promise<void> {
+    const query = this._relicItemQueryBase;
+    query.body.query.bool.must.push({
+      wildcard: {
+        Name_ja: `*マンダヴィル・*`,
+      },
+    });
     return new Promise((resolve, reject) => {
-      this._xivapiStore
-        .search({
-          indexes: 'item',
-          columns:
-            'ClassJobCategory.Name,ClassJobCategory.Name_de,ClassJobCategory.Name_en,ClassJobCategory.Name_fr,ClassJobCategory.Name_ja,Description_ja,ID,Icon,IconID,LevelEquip,LevelItem,Name,Name_de,Name_en,Name_fr,Name_ja,Patch,Url,UrlType',
-          body: {
-            query: {
-              bool: {
-                must: [
-                  {
-                    wildcard: {
-                      Name_ja: `*マンダヴィル・*`,
-                    },
-                  },
-                ],
-                must_not: [
-                  {
-                    wildcard: {
-                      Name_ja: `*レプリカ*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*防具*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*模造品*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*1回目の装着*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*装備不可*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*友好部族クエスト*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*スカイスチールツール*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*モーエンツール*`,
-                    },
-                  },
-                  {
-                    wildcard: {
-                      Description_ja: `*マンダヴィル*`,
-                    },
-                  },
-                ],
-              },
-            },
-            from: 0,
-            size: 1000,
-            sort: [],
-          },
-        })
-        .subscribe((searchItems) => {
-          console.log(searchItems);
-          this._items = this._items.concat(
-            (searchItems as any).Results as XivapiItemModel[]
-          );
-          resolve();
-        });
+      this._xivapiStore.search(query).subscribe((searchItems) => {
+        console.log(searchItems);
+        this._items = this._items.concat(
+          (searchItems as any).Results as XivapiItemModel[]
+        );
+        resolve();
+      });
     });
   }
 
