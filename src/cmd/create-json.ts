7509bfad2c4ce81c6056cea6ad9a13fb14b1cfcf
lastagous <from.iphone5s@gmail.com>
2023-12-06 23:57:21 +0900

    [ADD] Added missing quest and item information.

--- src/cmd/create-json.ts
+++ src/cmd/create-json.ts
@@ -129,8 +129,8 @@ const items = async () => {
     '[エウレカウェポン・ヒュダトス]',
     '[エウレカウェポン・ヒュダトス+1]',
     '[新たなエウレカウェポン]',
-    '[エウレカウェポン完成系]',
-    '[エウレカウェポンカスタム]',
+    '[エウレカウェポン完成形]',
+    '[エウレカウェポン・カスタム]',
   ].forEach((queryDescription) => {
     query.body.query.bool.should.push({
       match: {
@@ -181,6 +181,7 @@ const relations = () => {
   items.forEach((item) => {
     if (!item.ClassJobCategory.Name) return;
     if (!item.EquipSlotCategory.MainHand && !item.EquipSlotCategory.OffHand) return;
+    if (item.Name_ja.includes('未完の')) return;
 
     const result = {
       classJobCategory: item.ClassJobCategory,
@@ -190,7 +191,7 @@ const relations = () => {
         const regex = new RegExp(`.*：.*${item.Name_ja}{1}$|.*：${item.Name_ja}{1}＆.*`);
         return regex.test(achievement.achievement.name);
       }),
-    };
+    } as Relation;
 
     if (item.LevelEquip == 50) {
       if (item.Description_ja == '[古の武器・アニムス]') {
@@ -230,7 +231,11 @@ const relations = () => {
 
   for (let i = 0; i < resultPaladins.length; i = i + 2) {
     const paladin = resultPaladins[i];
-    paladin.items.push(resultPaladins[i + 1].items[0]);
+    try {
+      paladin.items.push(resultPaladins[i + 1].items[0]);
+    } catch (e) {
+      console.log(resultPaladins.map((r) => r.items.map((i) => i.Name_ja)));
+    }
     resultRelations.push(paladin);
   }
 
@@ -249,7 +254,7 @@ const relations = () => {
     });
   });
   // [info] fs relative path 'root' is execute command in folder
-  fs.writeFileSync('src/assets/data/local/confirm.json', JSON.stringify(confirm));
+  fs.writeFileSync('src/assets/data/local/confirm.json', JSON.stringify(confirm, null, 2));
 };
 
 const tooltips = async () => {
@@ -262,17 +267,24 @@ const tooltips = async () => {
 
   for (let item of items) {
     if (tooltips.find((tooltip) => tooltip.id == item.ID && tooltip.urlType == 'item')) continue;
-    const page = await superagent.get(
-      `${urlBase}item/?&${[
-        'db_search_category=item',
-        `category2=${item.EquipSlotCategory ? (item.EquipSlotCategory.MainHand == 1 ? 1 : 3) : ''}`,
-        `min_item_lv=${item.EquipSlotCategory ? item.LevelItem : ''}`,
-        `max_item_lv=${item.EquipSlotCategory ? item.LevelItem : ''}`,
-        `min_gear_lv=${item.EquipSlotCategory ? item.LevelEquip : ''}`,
-        `max_gear_lv=${item.EquipSlotCategory ? item.LevelEquip : ''}`,
-        `q=${encodeURIComponent(item.Name_ja)}`,
-      ].join('&')}`
-    );
+    const url = `${urlBase}item/?&${[
+      'db_search_category=item',
+      `category2=${item.EquipSlotCategory ? (item.EquipSlotCategory.MainHand == 1 ? 1 : 3) : ''}`,
+      `min_item_lv=${item.EquipSlotCategory ? item.LevelItem : ''}`,
+      `max_item_lv=${item.EquipSlotCategory ? item.LevelItem : ''}`,
+      `min_gear_lv=${item.EquipSlotCategory ? item.LevelEquip : ''}`,
+      `max_gear_lv=${item.EquipSlotCategory ? item.LevelEquip : ''}`,
+      `q=${encodeURIComponent(item.Name_ja)}`,
+    ].join('&')}`;
+    let page;
+    try {
+      page = await superagent.get(url);
+    } catch (e) {
+      console.log(`superagent.get(): failed. Wait 10 seconds and try again.`);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+      page = await superagent.get(url);
+    }
+
     const $ = cheerio.load(page.text);
     const tooltipId = $('.db-table__txt--detail_link').attr('href')?.split('/')[5];
     console.log(`${item.Name_ja} -> ${tooltipId}`);
@@ -291,9 +303,17 @@ const tooltips = async () => {
 
   for (let quest of quests) {
     if (tooltips.find((tooltip) => tooltip.id == quest.quest.id && tooltip.urlType == 'quest')) continue;
-    const page = await superagent.get(
-      `${urlBase}quest/?&${['db_search_category=quest', `q=${encodeURIComponent(quest.quest.name)}`].join('&')}`
-    );
+    const url = `${urlBase}quest/?&${['db_search_category=quest', `q=${encodeURIComponent(quest.quest.name)}`].join(
+      '&'
+    )}`;
+    let page;
+    try {
+      page = await superagent.get(url);
+    } catch (e) {
+      console.log(`superagent.get(): failed. Wait 10 seconds and try again.`);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+      page = await superagent.get(url);
+    }
     const $ = cheerio.load(page.text);
     const tooltipId = $('.db-table__txt--detail_link').attr('href')?.split('/')[5];
     console.log(`${quest.quest.name} -> ${tooltipId}`);
@@ -313,12 +333,18 @@ const tooltips = async () => {
   for (let achievement of achievements) {
     if (tooltips.find((tooltip) => tooltip.id == achievement.achievement.id && tooltip.urlType == 'achievement'))
       continue;
-    const page = await superagent.get(
-      `${urlBase}achievement/?&${[
-        'db_search_category=achievement',
-        `q=${encodeURIComponent(achievement.achievement.name)}`,
-      ].join('&')}`
-    );
+    const url = `${urlBase}achievement/?&${[
+      'db_search_category=achievement',
+      `q=${encodeURIComponent(achievement.achievement.name)}`,
+    ].join('&')}`;
+    let page;
+    try {
+      page = await superagent.get(url);
+    } catch (e) {
+      console.log(`superagent.get(): failed. Wait 10 seconds and try again.`);
+      await new Promise((resolve) => setTimeout(resolve, 10000));
+      page = await superagent.get(url);
+    }
     const $ = cheerio.load(page.text);
     const tooltipId = $('.db-table__txt--detail_link').attr('href')?.split('/')[5];
     console.log(`${achievement.achievement.name} -> ${tooltipId}`);
